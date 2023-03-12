9b7ebf1fa2da4f55c614411819fbceac83652175
joy <orjionyinyechi080@gmail.com>
2023-03-12 03:03:09 -0700

    changed

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue';
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
-import './assets/main.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import store from './store/Counter';
 import router  from './router/router';
